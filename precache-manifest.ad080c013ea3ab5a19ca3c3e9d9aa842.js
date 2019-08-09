6a21939b85dfa75c36a25121c17c5d9deb8f7430
stone-skipper <stone.skipper.ee@gmail.com>
2019-08-09 17:03:46 +0900

    Updates

---
+++ precache-manifest.ad080c013ea3ab5a19ca3c3e9d9aa842.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76b88354cf467077cc280c3e1fc6f03",
+    "url": "/svs/index.html"
+  },
+  {
+    "revision": "66cff7f1640151ad164d",
+    "url": "/svs/static/css/2.a589e0c0.chunk.css"
+  },
+  {
+    "revision": "1d9784be1bd98cd4b27e",
+    "url": "/svs/static/css/main.9ea986eb.chunk.css"
+  },
+  {
+    "revision": "66cff7f1640151ad164d",
+    "url": "/svs/static/js/2.ba20383d.chunk.js"
+  },
+  {
+    "revision": "1d9784be1bd98cd4b27e",
+    "url": "/svs/static/js/main.3838f04c.chunk.js"
+  },
+  {
+    "revision": "08d98e79ffb421772809",
+    "url": "/svs/static/js/runtime~main.b400508c.js"
+  },
+  {
+    "revision": "fc4e588938b9827ecd88a7fc8fdf2773",
+    "url": "/svs/static/media/content01.fc4e5889.jpg"
+  },
+  {
+    "revision": "c708a2b9ad80f7d5aca753262f129f5c",
+    "url": "/svs/static/media/content03.c708a2b9.jpg"
+  }
+]);
